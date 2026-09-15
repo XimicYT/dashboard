201fad74d151cdc8ce2eae4bb7e3ab168931ee65
XimicYT <abutts29@students.unorth.k12.in.us>
2026-09-15 09:10:55 -0400

    Delete .puppeteerrc.cjs

--- .puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-const { join } = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // Save Chrome inside the local project folder so Render packages it
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
